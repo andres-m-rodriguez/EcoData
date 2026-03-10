07031604fb28dcfee295fbde5e5ba06495918d9b
Andres <andresitomanuel@gmail.com>
2026-03-10 13:58:10 -0400

    Add locationsDb reference to ingestion service in AppHost

--- src/AquaTrack/EcoData.AquaTrack.Ingestion/Program.cs
+++ src/AquaTrack/EcoData.AquaTrack.Ingestion/Program.cs
@@ -1,13 +1,17 @@
-using EcoData.AquaTrack.Database.Extensions;
 using EcoData.AquaTrack.DataAccess.Extensions;
+using EcoData.AquaTrack.Database.Extensions;
 using EcoData.AquaTrack.Ingestion.Services;
 using EcoData.AquaTrack.Ingestion.Workers;
+using EcoData.Locations.DataAccess.Extensions;
+using EcoData.Locations.Database.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.AddServiceDefaults();
 builder.AddAquaTrackDatabase();
 builder.Services.AddAquaTrackDataAccess();
+builder.AddLocationsDatabase();
+builder.Services.AddLocationsDataAccess();
 
 builder.Services.AddHttpClient<IUsgsApiClient, UsgsApiClient>(client =>
 {
