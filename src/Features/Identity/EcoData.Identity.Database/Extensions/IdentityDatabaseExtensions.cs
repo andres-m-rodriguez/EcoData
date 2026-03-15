0c48b3f9502f53f3abd6f784460cfd6d974f3a73
Andres <andresitomanuel@gmail.com>
2026-03-15 19:02:02 -0400

    fix: Use AddAzureNpgsqlDbContext with NetTopologySuite for spatial data
    Refactor database extensions to use AddAzureNpgsqlDbContext which
    properly handles Entra ID authentication while also configuring
    NetTopologySuite for the Locations database spatial queries.
    
    This fixes the "geometry data type not found" error in production.

--- src/Features/Identity/EcoData.Identity.Database/Extensions/IdentityDatabaseExtensions.cs
+++ src/Features/Identity/EcoData.Identity.Database/Extensions/IdentityDatabaseExtensions.cs
@@ -1,6 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace EcoData.Identity.Database.Extensions;
@@ -9,43 +7,20 @@ public static class IdentityDatabaseExtensions
 {
     public static IHostApplicationBuilder AddIdentityDatabase(
         this IHostApplicationBuilder builder,
-        string connectionName = "identity"
-    )
+        string connectionName = "identity")
     {
-        var connectionString = builder.Configuration.GetConnectionString(connectionName);
-
-        builder.Services.AddDbContextPool<IdentityDbContext>(options =>
-        {
-            ConfigureDbContext(connectionString, options);
-        });
-
-        builder.Services.AddPooledDbContextFactory<IdentityDbContext>(options =>
-        {
-            ConfigureDbContext(connectionString, options);
-        });
-
-        // Enrich with Azure-specific configuration (SSL, health checks, telemetry)
-        builder.EnrichAzureNpgsqlDbContext<IdentityDbContext>();
+        builder.AddAzureNpgsqlDbContext<IdentityDbContext>(connectionName,
+            configureDbContextOptions: options =>
+            {
+                options.UseNpgsql(npgsqlOptions =>
+                {
+                    npgsqlOptions.MigrationsAssembly("EcoData.Identity.Database");
+                    npgsqlOptions.MigrationsHistoryTable("__ef_migrations_history", "public");
+                });
+                options.UseSnakeCaseNamingConvention();
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
 
         return builder;
-    }
-
-    private static void ConfigureDbContext(
-        string? connectionString,
-        DbContextOptionsBuilder options
-    )
-    {
-        options.UseNpgsql(
-            connectionString,
-            npgsqlOptions =>
-            {
-                npgsqlOptions.MigrationsAssembly("EcoData.Identity.Database");
-                npgsqlOptions.MigrationsHistoryTable("__ef_migrations_history", "public");
-                npgsqlOptions.EnableRetryOnFailure(maxRetryCount: 5);
-            }
-        );
-        options.UseSnakeCaseNamingConvention();
-        options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-        options.EnableThreadSafetyChecks(false);
     }
 }
