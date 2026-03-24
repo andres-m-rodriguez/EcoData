2a7d85c16b1d2235d59d7072005105b5075b6fa9
Andres <andresitomanuel@gmail.com>
2026-03-24 18:26:34 -0400

    Postgres db updated to use spacial

--- src/Features/Locations/EcoData.Locations.Database/Extensions/LocationsDatabaseExtensions.cs
+++ src/Features/Locations/EcoData.Locations.Database/Extensions/LocationsDatabaseExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Npgsql;
 
 namespace EcoData.Locations.Database.Extensions;
 
@@ -8,17 +9,22 @@ public static class LocationsDatabaseExtensions
 {
     public static IHostApplicationBuilder AddLocationsDatabase(
         this IHostApplicationBuilder builder,
-        string connectionName = "locations")
+        string connectionName = "locations"
+    )
     {
         // Use AddAzureNpgsqlDbContext for Entra ID auth support
-        builder.AddAzureNpgsqlDbContext<LocationsDbContext>(connectionName,
-            configureDbContextOptions: ConfigureOptions);
+        builder.AddAzureNpgsqlDbContext<LocationsDbContext>(
+            connectionName,
+            configureDbContextOptions: ConfigureOptions
+        );
 
         // Also register factory - required by repositories
-        builder.Services.AddPooledDbContextFactory<LocationsDbContext>((sp, options) =>
-        {
-            ConfigureOptions(options);
-        });
+        builder.Services.AddPooledDbContextFactory<LocationsDbContext>(
+            (sp, options) =>
+            {
+                ConfigureOptions(options);
+            }
+        );
 
         return builder;
     }
@@ -27,9 +33,11 @@ public static class LocationsDatabaseExtensions
     {
         options.UseNpgsql(npgsqlOptions =>
         {
-            npgsqlOptions.UseNetTopologySuite();
             npgsqlOptions.MigrationsAssembly("EcoData.Locations.Database");
             npgsqlOptions.MigrationsHistoryTable("__ef_migrations_history", "public");
+            // Configure the data source builder with NetTopologySuite before it's built
+            npgsqlOptions.ConfigureDataSource(dsBuilder => dsBuilder.UseNetTopologySuite());
+            npgsqlOptions.UseNetTopologySuite();
         });
         options.UseSnakeCaseNamingConvention();
         options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
