77057e7c02737dfc9b9e14f297a470b55f32fde5
andresm.rodriguezcan <andresm.rodriguezcan@gmail.com>
2026-04-21 12:10:08 -0400

    feat: Add FaunaFinder Blazor Web App for Wildlife features
    - Create FaunaFinder.Server project with Blazor Web App hosting
    - Create FaunaFinder.Client project with WASM components
    - Add MudBlazor for UI components
    - Configure Wildlife and Locations API endpoints
    - Add to AppHost with ExcludeFromManifest for local dev only

---
+++ src/Apps/FaunaFinder/FaunaFinder.Server/Program.cs
@@ -0,0 +1,46 @@
+using EcoData.Locations.Api;
+using EcoData.Locations.DataAccess.Extensions;
+using EcoData.Locations.Database.Extensions;
+using EcoData.Wildlife.Api;
+using EcoData.Wildlife.DataAccess;
+using EcoData.Wildlife.Database.Extensions;
+using FaunaFinder.Server.Components;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+builder.AddLocationsDatabase();
+builder.AddWildlifeDatabase();
+
+builder.Services.AddRazorComponents().AddInteractiveWebAssemblyComponents();
+builder.Services.AddMudServices();
+builder.Services.AddLocationsDataAccess();
+builder.Services.AddWildlifeDataAccess();
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(FaunaFinder.Client._Imports).Assembly);
+
+app.MapStateEndpoints();
+app.MapMunicipalityEndpoints();
+app.MapWildlifeApiEndpoints();
+
+app.Run();
